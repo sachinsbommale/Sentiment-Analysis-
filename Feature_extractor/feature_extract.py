091a83cf79f9e360f447a3e0e13b5b403dc696d1
Sachin Bommale <sachin113.sb@gmail.com>
2018-04-06 11:41:44 +0530

    Add files via upload

---
+++ Feature_extractor/feature_extract.py
@@ -0,0 +1,62 @@
+import re
+#initialize stopWords
+stopWords = []
+
+#start replaceTwoOrMore
+def replaceTwoOrMore(s):
+    #look for 2 or more repetitions of character and replace with the character itself
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", s)
+#end
+
+#start getStopWordList
+def getStopWordList(stopWordListFileName):
+    #read the stopwords file and build a list
+    stopWords = []
+    stopWords.append('AT_USER')
+    stopWords.append('URL')
+
+    fp = open(stopWordListFileName, 'r')
+    line = fp.readline()
+    while line:
+        word = line.strip()
+        stopWords.append(word)
+        line = fp.readline()
+    fp.close()
+    return stopWords
+#end
+
+#start getfeatureVector
+def getFeatureVector(tweet):
+    featureVector = []
+    #split tweet into words
+    words = tweet.split()
+    for w in words:
+        #replace two or more with two occurrences
+        w = replaceTwoOrMore(w)
+        #strip punctuation
+        w = w.strip('\'"?,.')
+        #check if the word stats with an alphabet
+        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
+        #ignore if it is a stop word
+        if(w in stopWords or val is None):
+            continue
+        else:
+            featureVector.append(w.lower())
+    return featureVector
+#end
+
+#Read the tweets one by one and process it
+fp = open('C:\\Users\\Sachin Bommale\\Desktop\\ADBS\\mysenti\\Minecraft.csv', 'r')
+line = fp.readline()
+
+st = open('C:\\Users\\Sachin Bommale\\Desktop\\ADBS\\mysenti\\Feature_extractor\\stopwords.txt', 'r')
+stopWords = getStopWordList('C:\\Users\\Sachin Bommale\\Desktop\\ADBS\\mysenti\\Feature_extractor\\stopwords.txt')
+
+while line:
+    processedTweet = processTweet(line)
+    featureVector = getFeatureVector(processedTweet)
+    print(featureVector)
+    line = fp.readline()
+#end loop
+fp.close()
